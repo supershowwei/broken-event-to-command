de967bd1c6ac821a9b39e7f0ed1110a6ff518e25
johnny <johnny@wantgoo.com>
2018-04-18 12:47:14 +0800

    broken EventToCommand

---
+++ EventToCommandApp/EventToCommandApp/MainPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Windows.Input;
+using Prism.Mvvm;
+using Xamarin.Forms;
+
+namespace EventToCommandApp
+{
+    public class MainPageViewModel : BindableBase
+    {
+        private ICommand navigatedCommand;
+
+        public ICommand NavigatedCommand
+        {
+            get
+            {
+                if (this.navigatedCommand == null)
+                {
+                    this.navigatedCommand = new Command(url => { Debug.WriteLine(url); });
+                }
+
+                return this.navigatedCommand;
+            }
+        }
+    }
+}
